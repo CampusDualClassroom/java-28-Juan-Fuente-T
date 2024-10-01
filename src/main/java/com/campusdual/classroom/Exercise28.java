ffc152a9a5c0d60cb7cc60989eddeda98d9854be
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-10-01 23:44:55 +0200

    Ejercicio 28 resuelto

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,20 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-
+        throwsException();
+        throwsArithmeticException();
     }
+
+    public static void throwsException() throws ArrayIndexOutOfBoundsException {
+        int[] myArray = new int[]{1, 2, 3};
+        int num = myArray[5];
+    }
+    public static void throwsArithmeticException(){
+        try{
+            int errorDivision = 50 / 0;
+        }catch (ArithmeticException e){
+            System.out.println("ArithmeticException: \n" + e);
+        }
+    }
+
 }
